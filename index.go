0c4d03e3d58c7ac69ff3abf4b675888c4f59d9c1
mamemamema <st.msnr.wk@gmail.com>
2021-10-12 22:43:07 +0900

    add go to db

--- index.go
+++ index.go
@@ -4,39 +4,39 @@ import (
  //   "os"
     "net/http"
     "github.com/gin-gonic/gin"
- //   "database/sql"
+    "database/sql"
     _ "github.com/go-sql-driver/mysql"
  //   "encoding/json"
 )
 
 type Band struct {
-    Id                    int       `json:"id"`
-    Overview              string    `json:"overview"`
-    Icon_id               int       `json:"icon_id"`
-    Introduction          string    `json:"introduction"`
-    Leader_user_id        int       `json:"leader_user_id"`
-    Member1_user_id       int       `json:"member1_user_id"`
-    Member2_user_id       int       `json:"member2_user_id"`
-    Member3_user_id       int       `json:"member3_user_id"`
-    Member4_user_id       int       `json:"member4_user_id"`
-    Member5_user_id       int       `json:"member5_user_id"`
-    Member6_user_id       int       `json:"member6_user_id"`
-    Member7_user_id       int       `json:"member7_user_id"`
-    Member8_user_id       int       `json:"member8_user_id"`
-    Member9_user_id       int       `json:"member9_user_id"`
-    Status_id             int       `json:"status_id"`
-    Activity_area         string    `json:"activity_area"`
-    Activity_direction_id int       `json:"activity_direction_id"`
-    Activity_date         string    `json:"activity_date"`
-    Online_flg            int       `json:"online_flg"`
-    Youtube_url           string    `json:"youtube_url"`
-    Soundcloud_url        string    `json:"soundcloud_url"`
-    Band_url              string    `json:"band_url"`
-    Genre                 string    `json:"genre"`
-    Rule_age              string    `json:"rule_age"`
-    Rule_sex              string    `json:"rule_sex"`
-    Recruitment_part      string    `json:"recruitment_part"`
-    Create_date           string    `json:"create_date"`
+  Id                    int       `json:"id"`
+  Overview              string    `json:"overview"`
+  Icon_id               int       `json:"icon_id"`
+  Introduction          string    `json:"introduction"`
+  Leader_user_id        int       `json:"leader_user_id"`
+  Member1_user_id       int       `json:"member1_user_id"`
+  Member2_user_id       int       `json:"member2_user_id"`
+  Member3_user_id       int       `json:"member3_user_id"`
+  Member4_user_id       int       `json:"member4_user_id"`
+  Member5_user_id       int       `json:"member5_user_id"`
+  Member6_user_id       int       `json:"member6_user_id"`
+  Member7_user_id       int       `json:"member7_user_id"`
+  Member8_user_id       int       `json:"member8_user_id"`
+  Member9_user_id       int       `json:"member9_user_id"`
+  Status_id             int       `json:"status_id"`
+  Activity_area         string    `json:"activity_area"`
+  Activity_direction_id int       `json:"activity_direction_id"`
+  Activity_date         string    `json:"activity_date"`
+  Online_flg            int       `json:"online_flg"`
+  Youtube_url           string    `json:"youtube_url"`
+  Soundcloud_url        string    `json:"soundcloud_url"`
+  Band_url              string    `json:"band_url"`
+  Genre                 string    `json:"genre"`
+  Rule_age              string    `json:"rule_age"`
+  Rule_sex              string    `json:"rule_sex"`
+  Recruitment_part      string    `json:"recruitment_part"`
+  Create_date           string    `json:"create_date"`
 }
 
 type Band_user struct {
@@ -82,6 +82,229 @@ type Test struct {
 }
 */
 
+var g_ch_bands_w chan int
+
+var g_bands []Band
+
+
+func refreshBandVal() {
+    var  id                    int    
+    var  overview              string 
+    var  icon_id               int    
+    var  introduction          string 
+    var  leader_user_id        int    
+    var  member1_user_id       int    
+    var  member2_user_id       int    
+    var  member3_user_id       int    
+    var  member4_user_id       int    
+    var  member5_user_id       int    
+    var  member6_user_id       int    
+    var  member7_user_id       int    
+    var  member8_user_id       int    
+    var  member9_user_id       int    
+    var  status_id             int    
+    var  activity_area         string 
+    var  activity_direction_id int    
+    var  activity_date         string 
+    var  online_flg            int    
+    var  youtube_url           string 
+    var  soundcloud_url        string 
+    var  band_url              string 
+    var  genre                 string 
+    var  rule_age              string 
+    var  rule_sex              string 
+    var  recruitment_part      string 
+    var  create_date           string 
+
+    var query_text string
+    var band Band
+
+    query_text = ""
+    query_text = query_text + "SELECT "
+    query_text = query_text + "  Id                    ,"
+    query_text = query_text + "  Overview              ,"
+    query_text = query_text + "  Icon_id               ,"
+    query_text = query_text + "  Introduction          ,"
+    query_text = query_text + "  Leader_user_id        ,"
+    query_text = query_text + "  Member1_user_id       ,"
+    query_text = query_text + "  Member2_user_id       ,"
+    query_text = query_text + "  Member3_user_id       ,"
+    query_text = query_text + "  Member4_user_id       ,"
+    query_text = query_text + "  Member5_user_id       ,"
+    query_text = query_text + "  Member6_user_id       ,"
+    query_text = query_text + "  Member7_user_id       ,"
+    query_text = query_text + "  Member8_user_id       ,"
+    query_text = query_text + "  Member9_user_id       ,"
+    query_text = query_text + "  Status_id             ,"
+    query_text = query_text + "  Activity_area         ,"
+    query_text = query_text + "  Activity_direction_id ,"
+    query_text = query_text + "  Activity_date         ,"
+    query_text = query_text + "  Online_flg            ,"
+    query_text = query_text + "  Youtube_url           ,"
+    query_text = query_text + "  Soundcloud_url        ,"
+    query_text = query_text + "  Band_url              ,"
+    query_text = query_text + "  Genre                 ,"
+    query_text = query_text + "  Rule_age              ,"
+    query_text = query_text + "  Rule_sex              ,"
+    query_text = query_text + "  Recruitment_part      ,"
+    query_text = query_text + "  Create_date           "
+    query_text = query_text + "FROM band"
+    query_text = query_text + ";"
+
+    // Create the database handle, confirm driver is present
+    db, _ := sql.Open("mysql", "xxxx:xxxxxx@/xxxx")
+    defer db.Close()
+ 
+    // Execute the query
+    results, err := db.Query(query_text)
+    if err != nil {
+        panic(err.Error()) // proper error handling instead of panic in your app
+    }
+ 
+    for results.Next() { 
+        err = results.Scan(
+          &id                   ,
+          &overview             ,
+          &icon_id              ,
+          &introduction         ,
+          &leader_user_id       ,
+          &member1_user_id      ,
+          &member2_user_id      ,
+          &member3_user_id      ,
+          &member4_user_id      ,
+          &member5_user_id      ,
+          &member6_user_id      ,
+          &member7_user_id      ,
+          &member8_user_id      ,
+          &member9_user_id      ,
+          &status_id            ,
+          &activity_area        ,
+          &activity_direction_id,
+          &activity_date        ,
+          &online_flg           ,
+          &youtube_url          ,
+          &soundcloud_url       ,
+          &band_url             ,
+          &genre                ,
+          &rule_age             ,
+          &rule_sex             ,
+          &recruitment_part     ,
+          &create_date                    
+        )
+        if err != nil {
+            panic(err.Error()) // proper error handling instead of panic in your app
+        }
+    
+        band = Band {
+          Id                    :id                   ,
+          Overview              :overview             ,
+          Icon_id               :icon_id              ,
+          Introduction          :introduction         ,
+          Leader_user_id        :leader_user_id       ,
+          Member1_user_id       :member1_user_id      ,
+          Member2_user_id       :member2_user_id      ,
+          Member3_user_id       :member3_user_id      ,
+          Member4_user_id       :member4_user_id      ,
+          Member5_user_id       :member5_user_id      ,
+          Member6_user_id       :member6_user_id      ,
+          Member7_user_id       :member7_user_id      ,
+          Member8_user_id       :member8_user_id      ,
+          Member9_user_id       :member9_user_id      ,
+          Status_id             :status_id            ,
+          Activity_area         :activity_area        ,
+          Activity_direction_id :activity_direction_id,
+          Activity_date         :activity_date        ,
+          Online_flg            :online_flg           ,
+          Youtube_url           :youtube_url          ,
+          Soundcloud_url        :soundcloud_url       ,
+          Band_url              :band_url             ,
+          Genre                 :genre                ,
+          Rule_age              :rule_age             ,
+          Rule_sex              :rule_sex             ,
+          Recruitment_part      :recruitment_part     ,
+          Create_date           :create_date          ,    
+        }
+
+        g_bands = append(g_bands, band)
+    }
+}
+
+func refreshBandUserVal() {
+
+  // Create the database handle, confirm driver is present
+  db, _ := sql.Open("mysql", "xxxx:xxxxxx@/xxxx")
+  defer db.Close()
+
+  // Execute the query
+  results, err := db.Query("SELECT ")
+  if err != nil {
+      panic(err.Error()) // proper error handling instead of panic in your app
+  }
+
+  for results.Next() {
+      var id int
+      var nm string
+
+      err = results.Scan(&id, &nm)
+      if err != nil {
+          panic(err.Error()) // proper error handling instead of panic in your app
+      }
+      
+
+  }
+}
+
+func refreshBandHtj() {
+
+  // Create the database handle, confirm driver is present
+  db, _ := sql.Open("mysql", "xxxx:xxxxxx@/xxxx")
+  defer db.Close()
+
+  // Execute the query
+  results, err := db.Query("SELECT ")
+  if err != nil {
+      panic(err.Error()) // proper error handling instead of panic in your app
+  }
+
+  for results.Next() {
+      var id int
+      var nm string
+
+      err = results.Scan(&id, &nm)
+      if err != nil {
+          panic(err.Error()) // proper error handling instead of panic in your app
+      }
+      
+
+  }
+}
+
+func refreshBandHtjMsgVal() {
+
+  // Create the database handle, confirm driver is present
+  db, _ := sql.Open("mysql", "xxxx:xxxxxx@/xxxx")
+  defer db.Close()
+
+  // Execute the query
+  results, err := db.Query("SELECT ")
+  if err != nil {
+      panic(err.Error()) // proper error handling instead of panic in your app
+  }
+
+  for results.Next() {
+      var id int
+      var nm string
+
+      err = results.Scan(&id, &nm)
+      if err != nil {
+          panic(err.Error()) // proper error handling instead of panic in your app
+      }
+      
+
+  }
+}
+
+
 func webServer() {
   r := gin.Default()
 
@@ -108,52 +331,9 @@ func webServer() {
   // JSON
   r.GET("/bands", func(c *gin.Context) {
 
-    var bands []Band
-    var band Band
     
-    band = Band {
-      Id                    :0         ,
-      Overview              :"string"  ,
-      Icon_id               :0         ,
-      Introduction          :"string"  ,
-      Leader_user_id        :0         ,
-      Member1_user_id       :0         ,
-      Member2_user_id       :0         ,
-      Member3_user_id       :0         ,
-      Member4_user_id       :0         ,
-      Member5_user_id       :0         ,
-      Member6_user_id       :0         ,
-      Member7_user_id       :0         ,
-      Member8_user_id       :0         ,
-      Member9_user_id       :0         ,
-      Status_id             :0         ,
-      Activity_area         :"string"  ,
-      Activity_direction_id :0         ,
-      Activity_date         :"string"  ,
-      Online_flg            :0         ,
-      Youtube_url           :"string"  ,
-      Soundcloud_url        :"string"  ,
-      Band_url              :"string"  ,
-      Genre                 :"string"  ,
-      Rule_age              :"string"  ,
-      Rule_sex              :"string"  ,
-      Recruitment_part      :"string"  ,
-      Create_date           :"string"  ,
-    }
 
-    bands = append(bands, band)
-    bands = append(bands, band)
-    bands = append(bands, band)
-    bands = append(bands, band)
-
-/*
-    test := Test {
-      Test : "aaaaaa"  ,
-      Abc  : "bbbbbbb" ,
-    }
-*/
-
-    c.JSON(http.StatusOK, bands)
+    c.JSON(http.StatusOK, g_bands)
   })
   r.GET("/bandusers", func(c *gin.Context) {
 
@@ -232,6 +412,12 @@ func main() {
 	dbpass = os.Getenv("DB_PASS")
 	dbnm = os.Getenv("DB_DTBS")
   */
+
+
+  go refreshBandVal()
+  go refreshBandUserVal()
+  go refreshBandHtj()
+  go refreshBandHtjMsgVal()
 
 	go webServer()
 
